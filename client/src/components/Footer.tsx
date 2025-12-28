595839967922fb9c8a1e28c90491456c7206ae92
Deepanshu <deepanshusaini502@gmail.com>
2025-12-28 17:08:15 +0530

    Updated Footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     return (
         <footer className="bg-background py-6 border-t border-gray-300 text-textSecondary text-center">
             <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between px-4">
-                <p className="text-sm dark:text-gray-400">&copy; {new Date().getFullYear()} PrimeDeal. All rights reserved.</p>
+                <p className="text-sm dark:text-gray-400">&copy; {new Date().getFullYear()} PrimeDeal by Deepanshu Saini. All rights reserved.</p>
                 
                 <div className="flex space-x-4 mt-3 md:mt-0 dark:text-gray-400">
                     <Facebook className="w-5 h-5 hover:text-brandGreen cursor-pointer" />
